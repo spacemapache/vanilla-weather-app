0856f101db01a2694902031a005f95daad3ed3a5
spacemapache <142768034+spacemapache@users.noreply.github.com>
2023-10-26 14:38:59 -0700

    updated time function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-// change weather information
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
 
 function showTemperature(response) {
   let humidity = Math.round(response.data.temperature.humidity);
@@ -10,12 +34,14 @@ function showTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let weatherDescription = document.querySelector("#description");
+  let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   humidityElement.innerHTML = `${humidity}%`;
   windElement.innerHTML = `${wind} km/h`;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
   weatherDescription.innerHTML = response.data.condition.description;
   iconElement.setAttribute(
     "src",
